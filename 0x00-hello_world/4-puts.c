cd2addd54b82e56bb4c8db3d96e7c712ada0cf61
Chinemerem Okonkwo <Engrchiok@gmail.com>
2023-08-21 08:50:34 -0700

    Updated task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include	<stdio.h>
 
 /**
- * 
+ * main - This is the call in function
  * puts - we are trying to print out a string with puts function
- *
+ * (0) - the value returned to the main
  */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
