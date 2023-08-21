ee63b6fe3aa591be7bb2d66a0999b7ef3c82012a
Chinemerem Okonkwo <Engrchiok@gmail.com>
2023-08-21 08:20:37 -0700

    completed task 3 and 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include	<stdio.h>
+
+/**
+ * 
+ * puts - we are trying to print out a string with puts function
+ *
+ */
+
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+
+	return 0;
+}
