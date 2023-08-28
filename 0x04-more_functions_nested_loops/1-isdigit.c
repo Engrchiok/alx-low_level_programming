83f5d1a8efeec933d904b52fff18970fc3a54a60
Chinemerem Okonkwo <Engrchiok@gmail.com>
2023-08-28 08:20:04 -0700

    Completed task 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+/**
+ * _isdigit - function definition for checking digits only
+ * C - parameter member (data) that the function checks
+ * Return: Always returns 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
