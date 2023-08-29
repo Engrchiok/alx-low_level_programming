59d023f38ee9118687e6ebcf7ab9b4327d33581d
Chinemerem Okonkwo <Engrchiok@gmail.com>
2023-08-29 03:44:29 -0700

    Completed task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - function definition for multiplyingtwo integers
+ * @a: first parameter for the mul function
+ * @b: second parameter for the mul function
+ * Return: Always returns int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
