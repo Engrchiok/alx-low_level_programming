4c7765791f02562472304eff7890827a6a2ebbfe
Chinemerem Okonkwo <Engrchiok@gmail.com>
2023-08-28 21:47:12 -0700

    Done all task

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+
+int _putchar(int a)
+{
+	return (write(1, &a, 1);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,7 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int _putchar(int c);
+void print_numbers(void);
 
 #endif
