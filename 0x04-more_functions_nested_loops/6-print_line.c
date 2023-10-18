af1db1cfb19740933e2d2abe97262a1f544f72e1
Chinemerem Okonkwo <engrchiok@gmail.com>
2023-10-18 17:03:28 +0100

    Completed task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - function definition for drawing a straight line in the terminal.
+ * @n - function parameter.
+ * _putchar - function call for printing single integers('charaters').
+ * Return: void.
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
