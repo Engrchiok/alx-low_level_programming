a48336ba09463bd623e82f876739987a812a7c49
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-01-17 14:31:53 +0100

    added task 0ls

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - function call.
+ * @argc: function parameter.
+ * @argv: function parameter.
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s \n", argv[0]);
-
-
+	return (0);
 }
