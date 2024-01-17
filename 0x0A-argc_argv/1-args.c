e044e2be7967f6b63b3443866349fe9fa1429a50
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-01-17 14:40:02 +0100

    added task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function call.
+ * @argc: function parameter.
+ * @argv: function parameter.
+ * Return: akwals 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
