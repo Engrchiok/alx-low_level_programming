a9265ae9c78801e55ea2ed6f8500f95422376e05
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-01-17 02:48:14 +0100

    Added task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,16 +4,24 @@
  * free_grid - function that frees a 2 dimensional grid.
  * @grid: function parameter.
  * @height: function parameter.
- * Return: no return.
+ * Return: void.
  */
 
 void free_grid(int **grid, int height)
 {
 	int i;
 
+	if (grid == NULL)
+	{
+		return;
+	}
+	else
+	{
 	free(grid);
 	for (i = 0; i  < height + 2; i++)
 	{
 		free(grid[i]);
+	}
+	return;
 	}
 }
