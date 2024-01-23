cd8237ca99ad616a4cae350faaca89cbde628aa6
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-01-23 11:26:33 +0100

    Done task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,12 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - name of structure type definition.
  * @name: struct character string variable.
  * @age: struct float variable.
  * @owner: struct character string variable.
  */
-
-#ifndef DOG_H
-#define DOG_H
 
 struct dog
 {
