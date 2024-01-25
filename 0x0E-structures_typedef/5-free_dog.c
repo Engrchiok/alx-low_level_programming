1503d85c071c422389744e295ffc23f605344720
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-01-25 15:52:28 +0100

    Completed task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - function definition that frees dogs.
+ * @d: function parameter.
+ * Return: nothing.
+ */
+
 void free_dog(dog_t *d)
 {
 	free(d);
