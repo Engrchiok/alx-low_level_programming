a9e4945ec3c55bf54e09719cf5457dbaf036130c
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-01-26 18:11:56 +0100

    Done task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - 
+ * Write a function that returns the sum of all its parameters.
+ * @n: function parameters.
+ * Return: always 0 or sum.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+	int sum;
+
+	if (n == 0)
+	{
+		return 0;
+	}
+	va_start (a, n);
+	for (i = 0, sum = 0; i < n; i++)
+	{
+		sum += va_arg (a, int);
+	}
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VARIA_F_H
+#define VARIA_F_H
+
+int sum_them_all(const unsigned int n, ...);
+void _putchar(char);
+
+#endif
