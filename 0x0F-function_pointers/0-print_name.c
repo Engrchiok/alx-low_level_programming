e076d1948685a11b4f91297f0b49f01f4d8833c8
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-01-26 10:30:54 +0100

    Done task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - function that manipulates a string, using a function pointer.
+ * @name: function parameter.
+ * @f: function pointer.
+ * Return: nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNC_P_H
+#define FUNC_P_H
+
+void _putchar(char);
+void print_name(char *name, void (*f)(char *));
+
+#endif
