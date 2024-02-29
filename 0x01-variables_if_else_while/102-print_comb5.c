85591518d0f289a240d9a87e9a71fc7da28a48ae
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-02-29 19:27:21 +0100

    Completed advanced task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function call.
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				if (c < a)
+				{
+				}
+				else
+				{
+					for (d = 48; d <= 57; d++)
+					{
+						if (c == a && d <= b)
+						{
+						}
+						else
+						{
+							putchar(a);
+							putchar(b);
+							putchar(32);
+							putchar(c);
+							putchar(d);
+							putchar(44);
+							putchar(32);
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
