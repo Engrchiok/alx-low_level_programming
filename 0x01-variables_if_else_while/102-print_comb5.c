8797f15e7798223d055ed313b5f99ea3d2e44266
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-02-29 19:45:13 +0100

    Completed advanced task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,34 +12,37 @@ int main(void)
 
 	for (a = 48; a <= 57; a++)
 	{
-		for (b = 48; b <= 57; b++)
+	for (b = 48; b <= 57; b++)
+	{
+	for (c = 48; c <= 57; c++)
+	{
+		if (c < a)
 		{
-			for (c = 48; c <= 57; c++)
+		}
+		else
+		{
+			for (d = 48; d <= 57; d++)
 			{
-				if (c < a)
+				if (c == a && d <= b)
 				{
 				}
 				else
 				{
-					for (d = 48; d <= 57; d++)
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+					if (!((a == 57) && (b == 56) && (c == 57) && (d == 57)))
 					{
-						if (c == a && d <= b)
-						{
-						}
-						else
-						{
-							putchar(a);
-							putchar(b);
-							putchar(32);
-							putchar(c);
-							putchar(d);
-							putchar(44);
-							putchar(32);
-						}
+						putchar(44);
+						putchar(32);
 					}
 				}
 			}
 		}
+	}
+	}
 	}
 	putchar(10);
 	return (0);
