0216f77a7c54cf8442dc0b8f3f3ccbee88c9984c
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-02-29 18:13:48 +0100

    completed advanced task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,29 +6,17 @@
  * Return: always 1.
  */
 
-int _strlen(char *);
-
 int main(void)
 {
-	char n = '\n', *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	write(2, s, sizeof(char) * _strlen(s));
-	write(1, &n, sizeof(char));
-	return (1);
-}
-
-/**
- * _strlen - function definition.
- * @s: function parameter.
- * Return: always len.
- */
-
-int _strlen(char *s)
-{
+	char n = '\n';
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int len;
+	char *p;
 
-	for (len = 0; *s != '\0'; s++, len++)
+	for (len = 0, p = s; *p != '\0'; p++, len++)
 	{
 	}
-	return (len);
+	write(2, s, sizeof(char) * len);
+	write(2, &n, sizeof(char));
+	return (1);
 }
