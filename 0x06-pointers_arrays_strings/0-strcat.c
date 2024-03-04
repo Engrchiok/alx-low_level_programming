a520fc5225bf92325a1d8a9bab6d0d5b979ee202
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-03-05 00:47:47 +0100

    uploaded the tasks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 /**
  * _strcat - function definition for concatenating two strings.
- * @dest: function argument for final output of concatenation.
- * @src: function argument for reference string to concatenate.
- * Return: always returns the pointer variable, dest, which is the concatenated string.
+ * @dest: function parameter.
+ * @src: function parameter.
+ * Return: always returns the pointer variable, dest.
  */
 
 char *_strcat(char *dest, char *src)
