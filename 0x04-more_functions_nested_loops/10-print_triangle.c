97b37c20b4fa6eef330dd3ddc5680c4c7c611290
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-03-04 19:27:27 +0100

    Completed task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * print_triangles - function definition.
+ * @size: function parameter.
+ * Return: always void.
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (i = 1, j = 1; size > 0; j++, size--)
+		{
+			k = j;
+			while (i < size)
+			{
+				_putchar(' ');
+				i++;
+			}
+			i = 1;
+			while (k > 0)
+			{
+				_putchar('#');
+				k--;
+			}
+			_putchar(10);
+		}
+	}
+	return;
+}
